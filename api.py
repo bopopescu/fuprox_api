3fd32ac4154d5302c01eae62c4674d2e1787abef
simons22d <denniskiruku@gmail.com>
2020-06-29 22:28:16 +0300

    github link

--- api.py
+++ api.py
@@ -304,7 +304,7 @@ def make_book():
         lipa_na_mpesapasskey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
         amount = 5
         party_b = business_shortcode
-        callback_url = "http://876f867d0178.ngrok.io/mpesa/b2c/v1"
+        callback_url = "http://68.183.89.127:8080/mpesa/b2c/v1"
 
         response = stk_push(token, business_shortcode, lipa_na_mpesapasskey, amount, phonenumber, party_b, phonenumber,
                             callback_url)
