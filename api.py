df9620f1b84b27fade05ec3de9b56929936cffff
simons22d <denniskiruku@gmail.com>
2020-07-02 18:42:35 +0300

    port

--- api.py
+++ api.py
@@ -316,16 +316,16 @@ def make_book():
         lipa_na_mpesapasskey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
         amount = 10
         party_b = business_shortcode
-        callback_url = "http://68.183.89.127/mpesa/b2c/v1"
+        callback_url = "http://68.183.89.127:8080/mpesa/b2c/v1"
 
         response = stk_push(token, business_shortcode, lipa_na_mpesapasskey, amount, phonenumber, party_b, phonenumber,callback_url)
-        booking = create_booking(service_name, start, branch_id, is_instant=is_instant, user_id=user_id)
-        print("booking", booking)
-        if booking:
-            final = generate_ticket(booking["id"])
-            sio.emit("online", {"booking_data": booking})
-        else:
-            final = {"msg": "Error generating the ticket. Please Try again later."}
+        # booking = create_booking(service_name, start, branch_id, is_instant=is_instant, user_id=user_id)
+        # print("booking", booking)
+        # if booking:
+        #     final = generate_ticket(booking["id"])
+        #     sio.emit("online", {"booking_data": booking})
+        # else:
+        #     final = {"msg": "Error generating the ticket. Please Try again later."}
     else:
         # we are going to request pay
         token_data = authenticate()
@@ -334,7 +334,7 @@ def make_book():
         lipa_na_mpesapasskey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
         amount = 5
         party_b = business_shortcode
-        callback_url = "http://68.183.89.127/mpesa/b2c/v1"
+        callback_url = "http://68.183.89.127:8080/mpesa/b2c/v1"
 
         try:
             stk_push(token, business_shortcode, lipa_na_mpesapasskey, amount, phonenumber, party_b, phonenumber,
