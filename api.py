f49e6c2dccefe5cbaf42c7333bf5cf5c118ecb3c
simons22d <denniskiruku@gmail.com>
2020-07-09 19:36:16 +0300

    instant tests

--- api.py
+++ api.py
@@ -339,9 +339,9 @@ def make_book_():
     is_instant_ = is_instant(token)
     if res["msg"]:
         if is_instant_:
-            final = make_booking(service_name, start, branch_id, True, user_id)
+            final = make_booking(service_name, start, branch_id, "Instant", user_id)
         else:
-            final = make_booking(service_name, start, branch_id, False, user_id)
+            final = make_booking(service_name, start, branch_id, None, user_id)
         # "result_code":res["result_desc"]
     else:
         final = {"result": "Token Provided Not Valid"}
